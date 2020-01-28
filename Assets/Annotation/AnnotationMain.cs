62f648c2e9500f8453abdd78081fb606cde7365e
Greg Bahm <grbahm@microsoft.com>
2020-01-27 19:19:55 -0800

    Adding annotation exploration scene

---
+++ Assets/Annotation/AnnotationMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnotationMain : MonoBehaviour
+{
+    [Range(1, 30)]
+    public float AnnotationDepth;
+    public Transform Annotation;
+
+    public AnnotationRepositioner AnnotationPlane;
+    public AnnotationRepositioner Phone;
+
+    private void Update()
+    {
+        Phone.UpdateSlatePositioning();
+
+        PlaceAnnotation();
+    }
+
+    private void PlaceAnnotation()
+    {
+        Annotation.localPosition = new Vector3(0, 0, AnnotationDepth * -.1f);
+        Annotation.localScale = new Vector3(AnnotationDepth, AnnotationDepth, AnnotationDepth);
+    }
+}
---
+++ Assets/Annotation/AnnotationRepositioner.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnnotationRepositioner : MonoBehaviour
+{
+    [SerializeField]
+    private BoxFocusable focus;
+    public BoxFocusable Focus { get { return this.focus; } }
+    
+    public float Smoothing;
+    private bool wasPinching;
+    
+    public bool CurrentlyRepositioning { get; private set; }
+    
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+    private Transform unsnappedTransform;
+
+    private void Start()
+    {
+        unsnappedTransform = new GameObject("Unsnapped Transform").transform;
+        unsnappedTransform.position = transform.position;
+        unsnappedTransform.rotation = transform.rotation;
+
+        targetPosition = transform.position;
+        targetRotation = transform.rotation;
+    }
+
+    public void OnEndResizing()
+    {
+        targetPosition = transform.position;
+        targetRotation = transform.rotation;
+    }
+
+    public void UpdateSlatePositioning()
+    {
+        bool pinching = MainPinchDetector.Instance.Pinching;
+        if (CurrentlyRepositioning)
+        {
+            if (pinching)
+            {
+                UpdatePinchPoint();
+            }
+            else
+            {
+                EndRepositioning();
+            }
+        }
+        bool shoulStartPinch = GetShouldStartGrab(pinching);
+        if (shoulStartPinch)
+        {
+            StartGrab();
+        }
+        UpdatePosition();
+        wasPinching = pinching;
+    }
+
+    private void UpdatePosition()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * Smoothing);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * Smoothing);
+    }
+
+    private void EndRepositioning()
+    {
+        CurrentlyRepositioning = false;
+        unsnappedTransform.parent = null;
+        Focus.ForceFocus = false;
+    }
+
+    private void UpdatePinchPoint()
+    {
+        targetPosition = unsnappedTransform.position;
+        targetRotation = unsnappedTransform.rotation;
+    }
+
+    private bool GetShouldStartGrab(bool pinching)
+    {
+        return pinching && !wasPinching && FocusManager.Instance.FocusedItem == Focus;
+    }
+
+    public void StartGrab()
+    {
+        CurrentlyRepositioning = true;
+        unsnappedTransform.position = transform.position;
+        unsnappedTransform.rotation = transform.rotation;
+        unsnappedTransform.parent = MainPinchDetector.Instance.PinchPoint;
+
+        Focus.ForceFocus = true;
+    }
+}
